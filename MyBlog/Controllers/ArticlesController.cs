93ad44c35b8965acc7f33130c43bbba39c746cee
laura <laura@DESKTOP-IR4CGFN>
2022-12-27 12:42:17 +0100

    article model build

--- MyBlog/Controllers/ArticlesController.cs
+++ MyBlog/Controllers/ArticlesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.Models;
 
 namespace MyBlog.Controllers
 {
@@ -6,7 +7,39 @@ namespace MyBlog.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // Créer une liste d'article EN DUR
+            var vm = new ArticlesViewModel
+            {
+                Articles = new List<ArticlesModel>
+                {
+                    new ArticlesModel
+                    {
+                        Id = 0,
+                        Title = "Les objets connectés en 2022",
+                        Content = "...",
+                        Available= true,
+                    } ,
+                    new ArticlesModel
+                    {
+                        Id = 1,
+                        Title = "Les objets connectés en 2023",
+                        Content = "...",
+                        Available= true,
+                    } ,
+                    new ArticlesModel
+                    {
+                        Id = 2,
+                        Title = "Les objets connectés en 2024",
+                        Content = "...",
+                        Available= false,
+                    } ,
+                }
+            };
+
+            //Chercher les articles ou tu veux
+            //Transforme en données pour ta vue (ici en viewModel)
+
+            return View(vm);
         }
     }
 }
---
+++ MyBlog/Models/ArticlesViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MyBlog.Models
+{
+    //Une classe que je veux donner à la vue
+    public class ArticlesViewModel
+    {
+        public List<ArticlesModel> Articles { get; set; }
+    }
+
+    //Une classe identique à ma table en bdd
+    public class ArticlesModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool Available { get; set; }
+    }
+}
