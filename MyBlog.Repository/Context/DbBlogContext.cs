8d5ab1704042b37e611a8099620b5d7432ad65d3
laura <laura@DESKTOP-IR4CGFN>
2022-12-28 10:52:31 +0100

    add entity

---
+++ MyBlog.Repository/Context/DbBlogContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Repository.Context
+{
+    public class DbBlogContext : DbContext
+    {
+        public DbBlogContext(DbContextOptions<DbBlogContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<ArticlesModel> Articles { get; set; }
+    }
+}
--- MyBlog/Program.cs
+++ MyBlog/Program.cs
@@ -1,9 +1,15 @@
 global using MyBlog.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using MyBlog.Repository.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Add Entity Framework 
+builder.Services.AddDbContext<DbBlogContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("BlogDbContext")));
 
 var mvcBuilder = builder.Services.AddRazorPages();
 
@@ -22,6 +28,16 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<DbBlogContext>();
+    context.Database.EnsureCreated();
+    // DbInitializer.Initialize(context);
+}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
